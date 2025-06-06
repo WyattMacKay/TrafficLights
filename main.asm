4fc5cbb80b78e7f2ec1059764306e3bd72e3f674
Sadiq Shahid <sadiq.shahid101@gmail.com>
2025-06-06 17:02:53 -0300

    Clean up and readability

--- main.asm
+++ main.asm
@@ -26,120 +26,131 @@ temp_print_state: .asciiz "Current state is: "
 #2. EW cross walk has cross request
 #3. Both cross walks have cross requests
 
-# ------------- Program initialization and defaults ---------------
+# -------------------------Program initialization and defaults----------------------
+#[ARGS]: 	No arguments
+#[RETURN]: 	No return
 init:
-li $s0, 0 	#lights state set to 0
-li $s1, 0 	#crosswalk input state 
-li $s2, 60 	#default speed limit
-li $s3, 3000 	#default green light time
-li $s4, 0 	#start time of simulator cycle
-#$s5 is reserved later
+	li $s0, 0 	#lights state set to 0
+	li $s1, 0 	#crosswalk input state 
+	li $s2, 60 	#default speed limit
+	li $s3, 3000 	#default green light time
+	li $s4, 0 	#start time of simulator cycle
+	li $s5, 0	#used for storing cycle time threshold
 
 
-# ------------ Gather introductory information for the simulator ----------------
+# ---------------Gather introductory information for the simulator------------------
+#[ARGS]: 	No arguments
+#[RETURN]: 	No return
 main:
-#Print welcome message and get user input
-li $v0, 4
-la $a0, welcome_msg
-syscall
-li $v0, 5
-syscall
-addi $t0, $v0, -1
-
-#Check input
-beq $t0, $zero, main_simulator
-addi $t0, $t0, -1
-beq $t0, $zero, set_speed_limit
-addi $t0, $t0, -1
-beq $t0, $zero, set_green_time
-
-#Input is invalid, print invalid message then try again
-li $v0, 4
-la $a0, invalid_input
-syscall
-j main
-
-# -------------- Setting the custom speed limit ------------------
-set_speed_limit:
-li $v0, 4
-la $a0, ask_speed_limit
-syscall
-li $v0, 5
-syscall
-move $s2, $v0
-
-#Input is accepted if new limit is greater than 0
-bgt $s2, $zero, main
-
-#Input is invalid, print invalid message then try again
-li $v0, 4
-la $a0, invalid_input
-syscall
-j set_speed_limit
-
-# -------------- Setting the custom green light time ------------------
-set_green_time:
-li $v0, 4
-la $a0, ask_green_time
-syscall
-li $v0, 5
-syscall
-move $s3, $v0
-
-#Input is accepted if new limit is greater than 0
-bgt $s3, $zero, main
-
-#Input is invalid, print invalid message then try again
-li $v0, 4
-la $a0, invalid_input
-syscall
-j set_green_time
-
-
-# ----------------------------- Starting the simulator ---------------------------------
-main_simulator:
-#get the time for the start of cycle
-li $v0, 30
-syscall
-move $s4, $a0
-
-rem $a0, $s0, 3 #if $t1 is 0, its a green light timer, if $t1 is 1 its a yellow timer, if $t1 is 2 its an all red timer
-
-#Find out if the remainder of state / 3 is 0, 1, or 2 then go to the corresponding threshold setting block
-#load params
-move $a1, $s2
-move $a2, $s3
-jal get_state_time
-move $s5, $v0
-
-rem $a0, $s0, 6		#set argument 0 to current light state
-move $a1, $s1		#set argument 1 to current crosswalk input state
-jal print_curr_state	#print the states
-move $s1, $v0		#save new crosswalk state from function call
-L1:
-	jal check_for_input
-	beq $v0, $zero, no_input
-		move $a0, $s1	#pass the current crosswalk input state as a paramater
-		move $a1, $v0	#pass the inputted character as a paramater
-		jal verify_input
-		move $s1, $v0
-		
-		#li $v0, 1
-		#move $a0, $s1
-		#syscall
-	
-	no_input:
-	li $v0, 30 #syscall to get current time into $a0
+	#Print welcome message and get user input
+	li $v0, 4
+	la $a0, welcome_msg
 	syscall
+	li $v0, 5
+	syscall
+	addi $t0, $v0, -1
+
+	#Check input
+	beq $t0, $zero, main_simulator
+	addi $t0, $t0, -1
+	beq $t0, $zero, set_speed_limit
+	addi $t0, $t0, -1
+	beq $t0, $zero, set_green_time
+
+	#Input is invalid, print invalid message then try again
+	li $v0, 4
+	la $a0, invalid_input
+	syscall
+	j main
+
+
+# ---------------------------Setting the custom speed limit-------------------------
+#[ARGS]: 	No arguments
+#[RETURN]: 	No return
+set_speed_limit:
+	li $v0, 4
+	la $a0, ask_speed_limit
+	syscall
+	li $v0, 5
+	syscall
+	move $s2, $v0
+
+	#Input is accepted if new limit is greater than 0
+	bgt $s2, $zero, main
+
+	#Input is invalid, print invalid message then try again
+	li $v0, 4
+	la $a0, invalid_input
+	syscall
+	j set_speed_limit
+
+
+# ---------------------- Setting the custom green light time -----------------------
+#[ARGS]: 	No arguments
+#[RETURN]: 	No return
+set_green_time:
+	li $v0, 4
+	la $a0, ask_green_time
+	syscall
+	li $v0, 5
+	syscall
+	move $s3, $v0
+
+	#Input is accepted if new limit is greater than 0
+	bgt $s3, $zero, main
+
+	#Input is invalid, print invalid message then try again
+	li $v0, 4
+	la $a0, invalid_input
+	syscall
+	j set_green_time
+
+
+# -----------------------------Starting the simulator-------------------------------
+#[ARGS]: 	No arguments
+#[RETURN]: 	No return
+main_simulator:
+	#get the time for the start of cycle
+	li $v0, 30
+	syscall
+	move $s4, $a0
+
+	#Find out if the remainder of state / 3 is 0, 1, or 2 then go to the corresponding threshold setting block
+	#remainder of 0 means its a green light, 1 means yellow light, 2 means all red
+	#load params
+	rem $a0, $s0, 3
+	move $a1, $s2
+	move $a2, $s3
+	jal get_state_time #call the function which returns the time to wait for the current cycle
+	move $s5, $v0
+
+	rem $a0, $s0, 6		#set argument 0 to current light state
+	move $a1, $s1		#set argument 1 to current crosswalk input state
+	jal print_curr_state	#print the states
+	move $s1, $v0		#save new crosswalk state from function call
+	L1:
+		jal check_for_input
+		beq $v0, $zero, no_input
+			move $a0, $s1	#pass the current crosswalk input state as a paramater
+			move $a1, $v0	#pass the inputted character as a paramater
+			jal verify_input
+			move $s1, $v0
+		
+		no_input:
+		li $v0, 30 #syscall to get current time into $a0
+		syscall
 	
-	sub $t0, $a0, $s4 #get the difference in time between the start of cycle and now
-	blt $t0, $s5, L1 #if the difference is less than the threshold, loop again
+		sub $t0, $a0, $s4 #get the difference in time between the start of cycle and now
+		blt $t0, $s5, L1 #if the difference is less than the threshold, loop again
 
-addi $s0, $s0, 1 #increment state
-j main_simulator #jump back up to print then loop again
+	addi $s0, $s0, 1 #increment state
+	j main_simulator #jump back up to print then loop again
 
-#-----------------------------------Crosswalk Input Check---------------------------
-check_for_input:	#No paramaters! Returns character, or 0
+
+#------------------------------Crosswalk Input Check--------------------------------
+#[ARGS]: 	No arguments
+#[RETURN]: 	Returns the character if there is input, or returns 0 if there is no input
+check_for_input:
 	li $v0, 0
 	lui $t0, 0xFFFF		#check if there is input
 	lw $t1, 0($t0)
@@ -151,7 +162,11 @@ check_for_input:	#No paramaters! Returns character, or 0
 	jr $ra
 
 
-verify_input:	#$a0 = Crosswalk input state	$a1 = character to verify  Returns new crosswalk state
+#-------------------------------Verify Latest Input---------------------------------
+#[ARGS]: 	$a0 = Crosswalk input state
+#		$a1 = Character to verify
+#[RETURN]: 	Returns updated crosswalk state
+verify_input: #Function checks if the latest input is a valid input (E, W, N, or S)
 	li $t0, 101	#e char
 	beq $a1, $t0, EWInput
 	li $t0, 119	#w char
@@ -170,27 +185,35 @@ verify_input:	#$a0 = Crosswalk input state	$a1 = character to verify  Returns ne
 		ori $v0, $a0, 1		#set the least significant bit to a 1
 		jr $ra
 	
-#----------------------------------Get State Time Function--------------------------
-get_state_time:	# $a0 = current state		$a1 = speed limit		$a2 = green light time
-beq $a0, $zero, thresh_set_green
-addi $a0, $a0, -1
-beq $a0, $zero, thresh_set_yellow
-#else, all must be red
-addi $v0, $zero, 1000 #when lights are all red, wait 1 second to go to next state
-jr $ra
+#-----------------------------Get State Time Function-------------------------------
+#[ARGS]: 	$a0 = Current state
+#		$a1 = Speed limit
+#		$a2 = Green light time
+#[RETURN]: 	Returns time to wait before going to next state
+get_state_time:
+	beq $a0, $zero, thresh_set_green
+	addi $a0, $a0, -1
+	beq $a0, $zero, thresh_set_yellow
+	
+	#else, all must be red
+		addi $v0, $zero, 1000 #when lights are all red, wait 1 second to go to next state
+		jr $ra
 
-thresh_set_green:
-add $v0, $a2, $zero #set the threshold to the green light delay then exit the if
-jr $ra
+	thresh_set_green:
+		add $v0, $a2, $zero #set the threshold to the green light delay then exit the if
+		jr $ra
 
-thresh_set_yellow:
-li $t0, 80		#80ms / (km/hr)
-mul $v0, $a1, $t0	#multiply km/hr by ms above	
-jr $ra
+	thresh_set_yellow:
+		li $t0, 80		#80ms / (km/hr)
+		mul $v0, $a1, $t0	#multiply km/hr by ms above	
+		jr $ra
 
 
-# -------------- Print the current state (TEMPORARY LOGIC) ------------------
-print_curr_state:		# $a0 = light state 	$a1 = crosswalk input state		Returns new crosswalk state
+# ---------------- Print the current state (TEMPORARY LOGIC) -----------------------
+#[ARGS]: 	$a0 = Current state
+#		$a1 = Crosswalk input state
+#[RETURN]: 	Returns new crosswalk state
+print_curr_state:
 	addi $sp, $sp, -4
 	sw $s0, 0($sp)	#stash $s0 so we can use it to hold our argument
 	
